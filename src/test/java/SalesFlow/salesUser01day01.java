43075f440311e560c9573c716cf510ae2fe526cd
Vijaykumar <vijaykumaralase@gmail.com>
2022-02-28 22:52:27 +0530

    GitsPractices user01 day01

---
+++ src/test/java/SalesFlow/salesUser01day01.java
@@ -0,0 +1,13 @@
+package SalesFlow;
+
+import org.testng.annotations.Test;
+
+public class salesUser01day01 {
+	
+	@Test
+	public void salesuser01method01()
+	{
+		System.out.println("This code is written by user01 on day 01");
+	}
+
+}
