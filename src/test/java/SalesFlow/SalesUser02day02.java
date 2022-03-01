125d411c9ef35c97162049a6bb1080e26a4c5431
Vijaykumar <vijaykumaralase@gmail.com>
2022-03-01 23:01:22 +0530

    This change has been done by user 02

---
+++ src/test/java/SalesFlow/SalesUser02day02.java
@@ -0,0 +1,13 @@
+package SalesFlow;
+
+import org.testng.annotations.Test;
+
+public class SalesUser02day02 {
+	
+	@Test
+	public void user02method01()
+	{
+		System.out.println("This code written by user02");
+	}
+
+}
